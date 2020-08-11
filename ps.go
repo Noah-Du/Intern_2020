bafa308a6b65a2d242e3f62e67d0731f803cac66
Noah-Du <68755690+Noah-Du@users.noreply.github.com>
2020-08-11 16:37:52 -0700

    Update and rename template.go to ps.go

---
+++ ps.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "os/exec"
+)
+
+func main() {
+    cmd := exec.Command("vctl","ps")
+    out, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+    }
+	fmt.Printf("%s\n", string(out))
+  //fmt.Println(cmd)
+  //out, err := cmd.CombinedOutput()
+  //outString := string(out)
+  //fmt.Println(outString)
+}
