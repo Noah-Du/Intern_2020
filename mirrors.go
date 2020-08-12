bd7712e1de0fed396770810e8cc1b3dac724a4a3
Noah-Du <68755690+Noah-Du@users.noreply.github.com>
2020-08-11 18:17:04 -0700

    Update mirrors.go

--- mirrors.go
+++ mirrors.go
@@ -5,11 +5,15 @@ import (
 	"os/exec"
 )
 
+// 1. 如何设置成多镜像线路可自己切换？
+
 func main() {
 	var name string
 	fmt.Println("请输入要下载的镜像名称及版本")
 	fmt.Scanln(&name)
-	cmd := exec.Command("vctl", "pull", "docker.mirrors.ustc.edu.cn/library/ubuntu")
+	name = "hub-mirror.c.163.com/library/" + name
+	fmt.Printf("%v", name)
+	cmd := exec.Command("vctl", "pull", name)
 	out, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err)
