12022de5eacb8ab44c68192c4c88608460c5c7ec
Noah-Du <68755690+Noah-Du@users.noreply.github.com>
2020-08-19 15:49:57 -0700

    Update mirrors.go
    Add error detection.
    If url starts with "http://", return error message and stop the program.
    If url does not end with "/library/", return error message and stop the program.

--- mirrors.go
+++ mirrors.go
@@ -3,21 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
 	//	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	// Create variable 'name' to store image's name, 'accelerator' to store registry mirror's url
 	var name string
 	var accelerator string
 
-	flag.StringVar(&name, "i", "", "镜像，默认为空")
-	flag.StringVar(&accelerator, "a", "hub-mirror.c.163.com/library/", "加速器url，默认为网易云加速器")
+	flag.StringVar(&name, "i", "", "image，set to empty")
+	flag.StringVar(&accelerator, "a", "hub-mirror.c.163.com/library/", "registry mirror url，set to 163yun")
 	flag.Parse()
 
+	if strings.HasPrefix(accelerator, "https://") {
+		fmt.Println("Registry Mirror address should not contain 'https://', please eliminate it.")
+		log.Fatal("Please fix the error and retry.")
+	}
+
+	if !strings.HasSuffix(accelerator, "/library/") {
+		fmt.Println("Registry Mirror address should end with '/library/', please fix it.")
+		log.Fatal("Please fix the error and retry.")
+	}
+
 	download := accelerator + name
-	fmt.Printf("下载路径为%v", download)
+	fmt.Printf("Download Address Is: %v \n", download)
 	cmd := exec.Command("vctl", "pull", download)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
